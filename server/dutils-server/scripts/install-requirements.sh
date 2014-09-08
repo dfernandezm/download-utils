aada8749eff03cc84897529363feb1b326880111
dfernandezm <morenza@gmail.com>
2014-09-09 00:38:29 +0100

    Starting config for the front end with Assetic. Problems with fonts inside CSS files and probably images not being found

---
+++ server/dutils-server/scripts/install-requirements.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+# detect if npm, uglify etc are installed
+npm install -g uglify-js
--- server/dutils-server/src/Morenware/DutilsBundle/Controller/DefaultController.php
+++ server/dutils-server/src/Morenware/DutilsBundle/Controller/DefaultController.php
@@ -3,10 +3,19 @@
 namespace Morenware\DutilsBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 
 class DefaultController extends Controller
 {
-    public function indexAction()
+	/**
+	 * Create instance.
+	 *
+	 * @Route("/home")
+	 * @Method("GET")
+	 *
+	 */
+	public function indexAction()
     {
         return $this->render('MorenwareDutilsBundle:Default:index.html.twig');
     }
