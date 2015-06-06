9f8a38c23bea2188e962a6da965a29104e6f38e1
dfernandezm <morenza@gmail.com>
2015-06-06 17:27:54 +0100

    Add basic way of replace characters with accents in renamed paths. This needs improvements (possible the rewrite of this script using PHP or other more powerful language)

--- app/scripts/multiple-rename-filebot.sh
+++ app/scripts/multiple-rename-filebot.sh
@@ -8,12 +8,12 @@ INPUT_ARRAY=%INPUT_PATHS%
 COUNT=${#INPUT_ARRAY[@]}
 # echo "Input Array: ${INPUT_ARRAY[*]} - COUNT"
 OUTPUT=%VIDEO_LIBRARY_BASE_PATH%
-
+TITLE_ESCAPED="{t.replaceAll(/[é]/,'e').replaceAll(/[á]/,'a').replaceAll(/[í]/,'i').replaceAll(/[ó]/,'o').replaceAll(/[ú]/,'ú'}"
 for ((i = 0; i < ${#INPUT_ARRAY[@]}; i++))
 do
   INPUT_PATH="${INPUT_ARRAY[$i]}"
   FILEBOT_AMC_CMD="$FB_EXEC -script fn:amc --output \"$OUTPUT\" --log-file $LOG_LOCATION --action $ACTION -non-strict \"$INPUT_PATH\" --def clean=y --conflict auto --def skipExtract=y"
-  FILEBOT_AMC_CMD="$FILEBOT_AMC_CMD --def unsorted=y --def \"seriesFormat=TV Shows/{n.upperInitial()}/{episode.special ? 'Specials':'Season '+s}/{n.upperInitial()} {episode.special ? '0xSpecial '+special.pad(2) : sxe.pad(2)} {t}\" \"movieFormat=Movies/{n} ({y})/{n}\""
+  FILEBOT_AMC_CMD="$FILEBOT_AMC_CMD --def unsorted=y --def \"seriesFormat=TV Shows/{n.upperInitial()}/{episode.special ? 'Specials':'Season '+s}/{n.upperInitial()} {episode.special ? '0xSpecial '+special.pad(2) : sxe.pad(2)} $TITLE_ESCAPED\" \"movieFormat=Movies/{n} ({y})/{n}\""
   let c=$COUNT-1
 
   if [ "$i" -eq "$c" ]; then
