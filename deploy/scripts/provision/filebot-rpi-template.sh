85e6f67c372b5e1d69f321f3ad7e63a114369106
dfernandezm <morenza@gmail.com>
2015-06-18 21:23:22 +0100

    Fix in Filebot installation to avoid errors with Unmappable characters in pathnames

--- deploy/scripts/provision/filebot-rpi-template.sh
+++ deploy/scripts/provision/filebot-rpi-template.sh
@@ -7,4 +7,4 @@ export LC_ALL="es_ES.UTF-8"
 JAVA_EXEC=%JAVA_EXEC%
 FILEBOT_HOME=%FILEBOT_HOME%
 
-$JAVA_EXEC -Dunixfs=false -DuseExtendedFileAttributes=true -DuseCreationDate=false -Dfile.encoding=UTF-8 -Dsun.net.client.defaultConnectTimeout=40000 -Dsun.net.client.defaultReadTimeout=120000 -Dapplication.deployment=ipkg -Duser.home=$FILEBOT_HOME/data -Dapplication.dir=$FILEBOT_HOME/data -Djava.io.tmpdir=$FILEBOT_HOME/data/temp -Djna.library.path=$FILEBOT_HOME -Djava.library.path=$FILEBOT_HOME -Dnet.filebot.AcoustID.fpcalc=$FILEBOT_HOME/fpcalc -jar $FILEBOT_HOME/FileBot.jar "$@"
+$JAVA_EXEC -Dunixfs=false -Dsun.jnu.encoding=UTF-8 -DuseExtendedFileAttributes=true -DuseCreationDate=false -Dfile.encoding=UTF-8 -Dsun.net.client.defaultConnectTimeout=40000 -Dsun.net.client.defaultReadTimeout=120000 -Dapplication.deployment=ipkg -Duser.home=$FILEBOT_HOME/data -Dapplication.dir=$FILEBOT_HOME/data -Djava.io.tmpdir=$FILEBOT_HOME/data/temp -Djna.library.path=$FILEBOT_HOME -Djava.library.path=$FILEBOT_HOME -Dnet.filebot.AcoustID.fpcalc=$FILEBOT_HOME/fpcalc -jar $FILEBOT_HOME/FileBot.jar "$@"
