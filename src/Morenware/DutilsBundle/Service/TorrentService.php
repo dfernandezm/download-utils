6c8d8b1af15be98f187d23e1bbe349b1820d7ae1
david <morenza@gmail.com>
2015-09-14 00:11:16 +0100

    Changed query to reduce amount of torrents being retrieved to the view. Do the same in develop branch.

--- src/Morenware/DutilsBundle/Service/TorrentService.php
+++ src/Morenware/DutilsBundle/Service/TorrentService.php
@@ -112,13 +112,12 @@ class TorrentService {
 		
 		$upperDate = new \DateTime();
 		$lowerDate = new \DateTime();
-		$interval = \DateInterval::createFromDateString("-5 years");
+		$interval = \DateInterval::createFromDateString("-1 week");
 		$lowerDate = $lowerDate->add($interval);
-//        $interval = \DateInterval::createFromDateString("+1 hour");
-//        $upperDate = $upperDate->add($interval);
 		
 		$q = $this->em->createQuery("select t from MorenwareDutilsBundle:Torrent t " . 
-				                    "where (t.dateStarted between :lowerDate and :upperDate) or t.state = 'AWAITING_DOWNLOAD' " .
+				                    "where (t.dateStarted is not null and t.dateStarted between :lowerDate and :upperDate) " .
+				                    "or t.state != 'COMPLETED' " .
 				                    "order by t.dateStarted DESC")
 		                            ->setParameter("lowerDate", $lowerDate->format("Y-m-d H:i"))
 		                            ->setParameter("upperDate", $upperDate->format("Y-m-d H:i"));
