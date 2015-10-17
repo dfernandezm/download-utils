06cc17078d89c70e43db3cf4923479b440d3a34d
david <morenza@gmail.com>
2015-10-17 01:45:11 +0100

    closes #81 : Deploy task initDb also backups the current DB before releasing

--- .mage/tasks/InitDatabase.php
+++ .mage/tasks/InitDatabase.php
@@ -6,18 +6,19 @@ use Mage\Console;
 class InitDatabase extends SymfonyAbstractTask {
 
   public function getName() {
-    return 'Init database if needed...';
+    return 'Init or backup database...';
   }
 
   public function run() {
     $realAppPath = str_replace("/console","",$this->getAppPath());
+    $backupPath = "db-backup";
     $yamlFile = $realAppPath . "/config/parameters.yml";
     $parameters = yaml_parse(file_get_contents($yamlFile));
     $dbHost = $parameters['parameters']['database_host'];
     $dbName = $parameters['parameters']['database_name'];
     $dbUser = $parameters['parameters']['database_user'];
     $dbPassword = $parameters['parameters']['database_password'];
-    $command = "cd deploy/scripts/database && sh install-db.sh $dbHost $dbName $dbUser $dbPassword";
+    $command = "cd deploy/scripts/database && sh install-db.sh $dbHost $dbName $dbUser $dbPassword $backupPath";
     $result = $this->runCommandRemote($command);
     return $result;
   }
--- deploy/scripts/database/install-db.sh
+++ deploy/scripts/database/install-db.sh
@@ -4,16 +4,28 @@ DBHOST=$1
 DBNAME=$2
 DBUSER=$3
 DBPASSWORD=$4
+BACKUP_PATH=$5
 
 mysql -u $DBUSER --password=$DBPASSWORD -e "USE $DBNAME"
 EXIT_CODE=$?
 
 if [ "$EXIT_CODE" -ne 0 ]; then
- echo "Creating db $DBNAME..."
+ echo "Database $DBNAME does not exist, creating db $DBNAME..."
  mysql -u $DBUSER --password=$DBPASSWORD -e "DROP DATABASE IF EXISTS $DBNAME"
  mysql -u $DBUSER --password=$DBPASSWORD -e "CREATE DATABASE $DBNAME"
  mysql -h $DBHOST -u $DBUSER --password=$DBPASSWORD -e "GRANT ALL ON *.* TO '$DBUSER'@'%'"
  mysql -h $DBHOST -u $DBUSER --password=$DBPASSWORD -e "UPDATE mysql.user SET Grant_priv='Y', Super_priv='Y' WHERE User='$DBUSER'"
  mysql -h $DBHOST -u $DBUSER --password=$DBPASSWORD -e "FLUSH PRIVILEGES"
+else
+
+ if [ -n "$BACKUP_PATH" ]; then
+   echo "Database $DBNAME exists, creating backup..."
+   mkdir -p $BACKUP_PATH
+   BACKUP_FILE="$BACKUP_PATH/db_backup.sql"
+   mysqldump -h$DBHOST -u$DBUSER -p$DBPASSWORD $DBNAME > $BACKUP_FILE
+   echo "Database $DBNAME backed up in $BACKUP_FILE"
+ else
+   echo "Database $DBNAME exists, but backup path not provided, skipping..."
+ fi
 
 fi
